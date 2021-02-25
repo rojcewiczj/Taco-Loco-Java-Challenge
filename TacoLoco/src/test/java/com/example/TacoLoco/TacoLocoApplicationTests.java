bbe4d8e12b26fc7a854c9d3c89f265a9ef7101bb
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-24 20:50:03 -0600

    initialized project using spring

---
+++ TacoLoco/src/test/java/com/example/TacoLoco/TacoLocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TacoLoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoLocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
